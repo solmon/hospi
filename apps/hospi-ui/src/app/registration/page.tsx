08477c7fce44a9129dfc3c1b1d38161fb2f94cd3
solmon <soraj@teksystems.com>
2023-07-04 21:07:44 +0530

    sample format

--- apps/hospi-ui/src/app/registration/page.tsx
+++ apps/hospi-ui/src/app/registration/page.tsx
@@ -1,16 +1,53 @@
 "use client"
 import {
-    Box
+  Box, Container, Stack, HStack, Text
 } from '@chakra-ui/react'
-  
+import {
+  Form,
+  FormLayout,
+  Field, SubmitButton, FormProps
+} from '@hp-ui/react'
+
 import { NextPage } from 'next'
-    
-  const RegistrationPage: NextPage = () => {
-    return (
-      <Box as="main" flex="1" py="2" px="4" overflowY="auto">
-        Your application content
-      </Box>
-    )
+
+const RegistrationPage: NextPage = () => {
+
+  const onSubmit1 = () => {
+
   }
-    
-  export default RegistrationPage
+  const props: FormProps = {
+    onSubmit: () => {
+
+    }
+  }
+
+  return (
+    <Box as="main" flex="1" py="2" px="4" overflowY="auto">
+      <Form
+        defaultValues={{
+          title: '',
+          description: '',
+        }}
+        {...props}
+        onSubmit={onSubmit1}
+      >
+        <FormLayout columns={[2, null, 4]}>
+          <Field
+            name="title"
+            label="Title"
+            rules={{ required: 'Title is required' }}
+          />
+          <Field
+            name="description"
+            label="Description"
+            rules={{ required: 'Description is required' }}
+          />
+
+          <SubmitButton />
+        </FormLayout>
+      </Form>
+    </Box>
+  )
+}
+
+export default RegistrationPage
